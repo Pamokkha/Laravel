6c43fae3eb39aba88be71062558301e556a864bc
Pamokkha <sawpamaukkha@gmail.com>
2025-07-14 18:37:32 -0500

    laravel lesson update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TemplateController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,14 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
 
-Route::get('/intros',[ \App\Http\Controllers\IntroController::class,'show']);
+Route::get('/intros', [App\Http\Controllers\IntroController::class, 'show']);
+Route::get('/test', [App\Http\Controllers\IntroController::class, 'testfun']);
+// delete two routes (about, contact)
 
-Route::get('/test', [\App\Http\Controllers\IntroController::class,'testfun']);
 
-Route::get('/about', [\App\Http\Controllers\TemplateController::class,'about']);
-Route::get('/post', [\App\Http\Controllers\TemplateController::class,'post']);
-Route::get('/contact', [\App\Http\Controllers\TemplateController::class,'contact']);
+Route::get('/', [TemplateController::class, 'index'])->name('homepage');
+Route::get('/about', [TemplateController::class, 'about'])->name('aboutpage');
+Route::get('/post', [TemplateController::class, 'post'])->name('postpage');
+Route::get('/contact', [TemplateController::class, 'contact'])->name('contactpage');
+
+// Backend
+Route::get('/table', [App\Http\Controllers\BackendTemplateController::class, 'tablefun'])->name('tablepage');
+
+Route::resource('categories', CategoryController::class);
+Route::resource('students', StudentController::class);
